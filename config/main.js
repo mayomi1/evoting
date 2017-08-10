5aaf44dbae2a86926ed03b343851d8d5b0a6dcc5
Ayandiran Mayomi <ayandiranmayomi@gmail.com>
2017-08-10 02:12:08 +0100

    Update main.js

--- config/main.js
+++ config/main.js
@@ -5,7 +5,6 @@ module.exports = {
     // Secret key for JWT signing and encryption
     'secret': 'thljnlbklk5t9',
     // Database connection information
-    //'database': 'mongodb://autocarecars:autocare@ds127949.mlab.com:27949/autocare-db',
     'database': 'mongodb://localhost:27017/evoting',
     // Setting port for server
     'port': process.env.PORT || 3000
